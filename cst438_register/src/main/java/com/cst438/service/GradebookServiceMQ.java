adfe87bfa44de39a8e50a685f0c7a3e7c561f9e9
Ksenia Kold <kseniacold@gmail.com>
2022-09-26 22:08:35 -0700

    feat: add logic for gradebook service MQ

--- cst438_register/src/main/java/com/cst438/service/GradebookServiceMQ.java
+++ cst438_register/src/main/java/com/cst438/service/GradebookServiceMQ.java
@@ -1,57 +1,63 @@
 package com.cst438.service;
 
 
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.cst438.domain.CourseDTOG;
 import com.cst438.domain.Enrollment;
 import com.cst438.domain.EnrollmentDTO;
 import com.cst438.domain.EnrollmentRepository;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 
 public class GradebookServiceMQ extends GradebookService {
-	
-	@Autowired
-	RabbitTemplate rabbitTemplate;
-	
-	@Autowired
-	EnrollmentRepository enrollmentRepository;
-	
-	@Autowired
-	Queue gradebookQueue;
-	
-	
-	public GradebookServiceMQ() {
-		System.out.println("MQ grade book service");
-	}
-	
-	// send message to grade book service about new student enrollment in course
-	@Override
-	public void enrollStudent(String student_email, String student_name, int course_id) {
-		 
-		EnrollmentDTO enrollmentDTO = new EnrollmentDTO(student_email, student_name, course_id);
-		
-		rabbitTemplate.convertAndSend(gradebookQueue.getName(), enrollmentDTO);
-		
-		System.out.println("Message send to gradbook service for student "+ student_email +" " + course_id);  
-		
-	}
-	
-	@RabbitListener(queues = "registration-queue")
-	public void receive(CourseDTOG courseDTOG) {
-		System.out.println("Receive enrollment :" + courseDTOG);
 
-		// process the list of student grades
-		for (CourseDTOG.GradeDTO g : courseDTOG.grades) {
-			Enrollment e = enrollmentRepository.findByEmailAndCourseId(g.student_email, courseDTOG.course_id);
-			e.setCourseGrade(g.grade);
-			enrollmentRepository.save(e);
-			System.out.println("final grade update " + g.student_email + " " + courseDTOG.course_id + " " + g.grade);
-		}
-	}
+    @Autowired
+    RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    EnrollmentRepository enrollmentRepository;
+
+    @Autowired
+    Queue gradebookQueue;
+
+
+    public GradebookServiceMQ() {
+        System.out.println("MQ grade book service");
+    }
+
+    // send message to grade book service about new student enrollment in course
+    @Override
+    public void enrollStudent(String student_email, String student_name, int course_id) {
+        EnrollmentDTO enrollmentDTO = new EnrollmentDTO();
+        enrollmentDTO.studentEmail = student_email;
+        enrollmentDTO.studentName = student_name;
+        enrollmentDTO.course_id = course_id;
+
+        rabbitTemplate.convertAndSend(gradebookQueue.getName(), enrollmentDTO);
+
+        System.out.println("Message sent to gradebook service: " + enrollmentDTO);
+    }
+
+    @RabbitListener(queues = "registration-queue")
+    public void receive(CourseDTOG courseDTO) {
+        System.out.println("Receive course DTO data :" + courseDTO);
+
+        // update student grades
+        for (CourseDTOG.GradeDTO gradeDTO : courseDTO.grades) {
+            Enrollment enrollment = enrollmentRepository.findByEmailAndCourseId(gradeDTO.student_email, courseDTO.course_id);
+
+            if (enrollment == null) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No enrollment found for student with provided email and course id");
+            }
+
+            enrollment.setCourseGrade(gradeDTO.grade);
+            enrollmentRepository.save(enrollment);
+            System.out.println("Final grade updated: " + gradeDTO.student_email + " " + courseDTO.course_id + " " + gradeDTO.grade);
+        }
+    }
 
 }
